8c15c73f46804b656af32141856bc4733d96e7f7
AhmadProjects-git <ahmadhassanofficala@gmail.com>
2026-07-13 10:59:20 +0500

    fix: add server listener to keep container alive

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
-// Real Web Route
+// Main Endpoint
 app.get('/', (req, res) => {
     res.status(200).send("Welcome to Daraz Clone - Enterprise Edition");
+});
+
+// ⚠️ Zaroori Command: Jo server ko background mein continuous chalati rakhegi
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
